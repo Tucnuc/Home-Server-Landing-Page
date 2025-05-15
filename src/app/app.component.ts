725e39559dc67507bf0bfd1e55143765d7d10379
Tucnuc <cer42372@spseol.cz>
2025-05-15 18:57:48 +0200

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+export type Button = {
+  icon: string;
+  name: string;
+  color: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Deutsch-Server';
+
+  apiKey: string = '';
+  fetched: boolean = false;
+  loading: boolean = true;
+
+  nameValues = {
+    default: 'Olomouc',
+    searchName: '',
+    display: 'Olomouc'
+  }
+
+  weatherData: any = {};
+  
+  weatherImages: {[key: string]: string} = {
+    Clear: './images/sunny.png',
+    Clouds: './images/cloudy.png',
+    Rain: './images/rainy.png',
+    Snow: './images/snowy.png',
+    Haze: './images/cloudy.png',
+    Mist: './images/cloudy.png',
+  }
+  backgroundImages: {[key: string]: string} = {
+    Clear: 'linear-gradient(to right, #f3b07c, #fcd283)',
+    Clouds: 'linear-gradient(to right, #57d6d4, #71eeec)',
+    Rain: 'linear-gradient(to right, #5bc8fb, #80eaff)',
+    Snow: 'linear-gradient(to right, #aff2ff, #fffff)',
+    Haze: 'linear-gradient(to right, #57d6d4, #71eeec)',
+    Mist: 'linear-gradient(to right, #57d6d4, #71eeec)',
+  }
+
+  currentDate = new Date()
+  daysOfWeek = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+  months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+
+  async search() {
+    if (this.nameValues.searchName.trim() == "") return
+    this.loading = true;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.nameValues.searchName}&units=Metric&appid=${this.apiKey}`;
+    const res = await fetch(url);
+
+    this.nameValues.display = this.nameValues.searchName;
+    this.nameValues.searchName = "";
+
+    const searchData = await res.json();
+    if (searchData.cod !== 200) this.weatherData = {notFound: true};
+    else {
+      this.weatherData = searchData;
+    }
+    this.loading = false;
+  }
+  async defaultRequest() {
+    if (this.fetched) return;
+    this.fetched = true;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.nameValues.default}&units=Metric&appid=${this.apiKey}`;
+    const res = await fetch(url);
+
+    const searchData = await res.json();
+    if (searchData.cod !== 200) this.weatherData = {notFound: true};
+    else {
+      this.weatherData = searchData;
+    }
+    this.loading = false;
+  }
+
+  ngOnInit(): void {
+    this.defaultRequest();
+  }
+
+  buttons = signal<Button[]>([
+    { icon: './svgs/Nextcloud_Logo.svg', name: 'Nextcloud', color: 'linear-gradient(to right, white, #87CEEB)', link: 'https://nextcloud.dojc.cc/' },
+    { icon: './svgs/CodeServer_Logo.svg', name: 'Code Server', color: 'linear-gradient(to right, #559CFC, #086BFD)', link: 'https://code.dojc.cc/' },
+  ]);
+
+  openLink(url: string) { window.open(url, '_blank'); }
 }
